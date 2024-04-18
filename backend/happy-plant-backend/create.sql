b935addb0f60bd8cfc54d8846d1d9c229f35508e
Katharina Reiner <katharina.reiner@hof-university.de>
2024-04-18 13:57:41 +0200

    feat: db initializer

--- backend/happy-plant-backend/create.sql
+++ backend/happy-plant-backend/create.sql
@@ -1,4 +1,4 @@
-create table bars (id uuid not null, name varchar(255), primary key (id));
+create table bars (id uuid not null, description varchar(255), name varchar(255), primary key (id));
 create table foos (id uuid not null, name varchar(255), primary key (id));
 create table foos_bars (bar_id uuid not null, foo_id uuid not null);
 alter table if exists foos_bars add constraint FKj5h5xveakymds97ridwir261k foreign key (bar_id) references bars;
