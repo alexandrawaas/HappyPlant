2981166c771b38e6c847e0797a8ce1228c84ed05
ralfm <ralf.manig@hof-university.de>
2024-04-21 19:09:35 +0200

    Controllerklassen angelegt

---
+++ backend/happy-plant-backend/src/main/kotlin/com/happyplant/backend/controllers/SpeciesController.kt
@@ -0,0 +1,11 @@
+package com.happyplant.backend.controllers
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
+@RequestMapping("species")
+class SpeciesController {
+
+}
