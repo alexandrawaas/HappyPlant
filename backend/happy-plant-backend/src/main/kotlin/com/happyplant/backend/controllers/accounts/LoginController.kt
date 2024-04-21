24a9525b8eb654bd63f25e6aefacb6d610c0a223
ralfm <ralf.manig@hof-university.de>
2024-04-21 19:09:35 +0200

    Controllerklassen angelegt

---
+++ backend/happy-plant-backend/src/main/kotlin/com/happyplant/backend/controllers/accounts/LoginController.kt
@@ -0,0 +1,10 @@
+package com.happyplant.backend.controllers.accounts
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
+@RequestMapping("login")
+class LoginController {
+}
