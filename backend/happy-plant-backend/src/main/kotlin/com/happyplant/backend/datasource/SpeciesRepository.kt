7288f290d586111d1c0111f45bca316070976bc9
ralfm <ralf.manig@hof-university.de>
2024-04-25 21:38:38 +0200

    prepare rebase

---
+++ backend/happy-plant-backend/src/main/kotlin/com/happyplant/backend/datasource/SpeciesRepository.kt
@@ -0,0 +1,4 @@
+package com.happyplant.backend.datasource
+
+class SpeciesRepository {
+}
--- backend/happy-plant-backend/src/main/kotlin/com/happyplant/backend/model/types/LightingType.kt
+++
@@ -1,9 +0,0 @@
-package com.happyplant.backend.model.types
-
-enum class LightingType {
-    //ordinals are used for accumulation
-    FULL_SHADE, //0
-    PART_SHADE,  //1
-    SUN,  //2
-    FULL_SUN  //3
-}
