6991dcb9159c9b56354a0dabba621accd695a362
Katharina Reiner <katharina.reiner@hof-university.de>
2024-05-01 13:29:52 +0200

    feat: model functionalities (not correctly working yet)

---
+++ backend/happy-plant-backend/src/main/kotlin/com/happyplant/backend/model/types/LightingType.kt
@@ -0,0 +1,18 @@
+package com.happyplant.backend.model.types
+
+import kotlin.math.max
+import kotlin.math.min
+
+enum class LightingType {
+    //ordinals are used for accumulation
+    FULL_SHADE, //0
+    PART_SHADE,  //1
+    SUN,  //2
+    FULL_SUN;  //3
+
+    operator fun plus(other: LightingType): LightingType =
+        LightingType.values()[min((this.ordinal + other.ordinal), values().size-1)]
+
+    operator fun dec() =
+        LightingType.values()[max((this.ordinal - 1),0)]
+}
