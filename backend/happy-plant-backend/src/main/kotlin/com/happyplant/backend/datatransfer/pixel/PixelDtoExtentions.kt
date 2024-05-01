548e262e8f7965811754ab89a373da2606926926
Katharina Reiner <katharina.reiner@hof-university.de>
2024-05-01 13:29:52 +0200

    feat: model functionalities (not correctly working yet)

--- backend/happy-plant-backend/src/main/kotlin/com/happyplant/backend/datatransfer/pixel/PixelDtoExtentions.kt
+++ backend/happy-plant-backend/src/main/kotlin/com/happyplant/backend/datatransfer/pixel/PixelDtoExtentions.kt
@@ -1,7 +1,8 @@
 package com.happyplant.backend.datatransfer.pixel
 
 import com.happyplant.backend.model.Pixel
-import com.happyplant.backend.datatransfer.room.asDtoResponse
+import com.happyplant.backend.model.Room
+import com.happyplant.backend.services.RoomsService
 
 fun Pixel.asDtoResponse(): PixelDto =
     PixelDto(
@@ -13,4 +14,16 @@ fun Pixel.asDtoResponse(): PixelDto =
         lightingType = this.lightingType,
         plants = listOf(), // TODO
         // plants = this.plants.map { it.asDtoResponse() }
+    )
+
+fun PixelDto.asEntity(roomService: RoomsService): Pixel =
+    Pixel(
+        id = this.id,
+        x = this.x,
+        y = this.y,
+        room = roomService.getRoom(this.roomId) ?: throw NoSuchElementException(),
+        isWindow = this.isWindow,
+        lightingType = this.lightingType,
+        plants = listOf(), // TODO
+        // plants = this.plants.map { it.asEntity() }
     )
