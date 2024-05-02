77060059d50892890bb4f767a007b088bffb9caf
Katharina Reiner <katharina.reiner@hof-university.de>
2024-05-02 20:41:48 +0200

    fix: Bean not found error (change @Repository and @Service annotations)

--- backend/happy-plant-backend/src/main/kotlin/com/happyplant/backend/repository/RoomRepository.kt
+++ backend/happy-plant-backend/src/main/kotlin/com/happyplant/backend/repository/RoomRepository.kt
@@ -1,11 +1,11 @@
 package com.happyplant.backend.repository
 
 import com.happyplant.backend.model.Room
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 import java.util.UUID
 
-@Repository
-interface RoomRepository: CrudRepository<Room, UUID> {
+interface RoomRepository: JpaRepository<Room, UUID> {
     fun findAllByName(name: String): Iterable<Room>
 }
