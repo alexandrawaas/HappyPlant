af3122b7f0b8e4adfbe462c4c3abd9ae737127a8
Katharina Reiner <katharina.reiner@hof-university.de>
2024-05-02 20:41:48 +0200

    fix: Bean not found error (change @Repository and @Service annotations)

--- backend/happy-plant-backend/src/main/kotlin/com/happyplant/backend/repository/UserRepository.kt
+++ backend/happy-plant-backend/src/main/kotlin/com/happyplant/backend/repository/UserRepository.kt
@@ -5,7 +5,6 @@ import java.util.UUID
 import com.happyplant.backend.model.User
 import org.springframework.stereotype.Repository
 
-@Repository
 interface UserRepository : JpaRepository<User, UUID> {
     fun findByEmail(email: String): User?
     fun findByResetPasswordToken(resetPasswordToken: String): User?
--- backend/happy-plant-backend/src/main/kotlin/com/happyplant/backend/service/UserService.kt
+++ backend/happy-plant-backend/src/main/kotlin/com/happyplant/backend/service/UserService.kt
@@ -3,8 +3,10 @@ package com.happyplant.backend.service
 import com.happyplant.backend.repository.UserRepository
 import com.happyplant.backend.datatransfer.user.NotificationSettingsDtoRequest
 import com.happyplant.backend.model.User
+import org.springframework.stereotype.Service
 import java.util.*
 
+@Service
 class UserService (
     private val db: UserRepository
 ) {
