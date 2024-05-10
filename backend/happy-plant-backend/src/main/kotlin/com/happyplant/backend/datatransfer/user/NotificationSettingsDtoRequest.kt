2eeafcb7cf444330483ebf47914aa0579871c181
Ralf Manig <ralf.manig@hof-university.de>
2024-05-10 09:41:15 +0200

    scheduled testcalls every 15 min

--- backend/happy-plant-backend/src/main/kotlin/com/happyplant/backend/datatransfer/user/NotificationSettingsDtoRequest.kt
+++ backend/happy-plant-backend/src/main/kotlin/com/happyplant/backend/datatransfer/user/NotificationSettingsDtoRequest.kt
@@ -3,9 +3,9 @@ package com.happyplant.backend.datatransfer.user
 import java.time.LocalTime
 
 data class NotificationSettingsDtoRequest(
-        val receivePushNotifications: Boolean,
-        val pushNotificationToken: String?,
-        val pushNotificationsTime: LocalTime?,
+    val receivePushNotifications: Boolean,
+    val pushNotificationToken: String?,
+    val pushNotificationsTime: LocalTime?,
 ) {
 
 }
