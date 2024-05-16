599193f31b4a06ab524c70e1b5b8b51f200b03ca
Katharina Reiner <katharina.reiner@hof-university.de>
2024-05-16 21:58:04 +0200

    feat: inventory

--- frontend/HappyPlant/screens/room/SingleRoomInventory.js
+++ frontend/HappyPlant/screens/room/SingleRoomInventory.js
@@ -1,7 +1,75 @@
-import { Text } from "react-native-elements"
+import { Text, StyleSheet, View, TouchableOpacity } from "react-native"
+import CollapsibleBar from "../other/CollapsibleBar"
+import { LinearGradient } from "expo-linear-gradient"
+import { useState, useEffect } from "react"
+import fetchURL from "./RoomService"
+import InventoryItem from "./InventoryItem"
 
-export default function SingleRoomInventory() {
+export default function SingleRoomInventory({ room }) {
+    const [inventory, setInventory] = useState();
+
+    useEffect(() => {
+        fetchURL(`/inventory`, 'GET', setInventory);
+    }, [])
+
+    const handleAddPlantToInventoryPress = () => {
+        console.log("TODO: implement create plant dialog")
+    }
+
     return (
-        <Text>Inventory</Text>
+        <View style={styles.container}>
+            <CollapsibleBar title="Inventar">
+                <LinearGradient colors={['#fdfbef', '#fef1ed']} style={styles.detailContainer}>
+                    <View style={styles.plantsContainer}>
+                        {inventory?.map(p =>
+                            <InventoryItem plant={p} />
+                        )}
+                    </View>
+                    <View style={styles.addButtonContainer}>
+                        <TouchableOpacity onPress={handleAddPlantToInventoryPress}>
+                            <LinearGradient colors={['#fdfbef', '#fef1ed']} style={[styles.button, styles.shadowed]}>
+                                <Text>+</Text>
+                            </LinearGradient>
+                        </TouchableOpacity>
+                    </View>
+                </LinearGradient>
+            </CollapsibleBar>
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    shadowed: {
+        elevation: 4,
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 4 },
+        shadowOpacity: 0.3,
+        shadowRadius: 4,
+    },
+    container: {
+        backgroundColor: '#fdfbef',
+        borderRadius: 15,
+        marginBottom: 15,
+    },
+    detailContainer: {
+        borderRadius: 15,
+        padding: 15,
+    },
+    plantsContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        flexWrap: "wrap",
+        justifyContent: 'center'
+    },
+    addButtonContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center'
+    },
+    button: {
+        paddingHorizontal: 20,
+        paddingVertical: 2.5,
+        borderRadius: 5,
+        marginTop: 5,
+    }
+})
