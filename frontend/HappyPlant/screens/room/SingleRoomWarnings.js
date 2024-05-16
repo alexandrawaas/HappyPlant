c69170c9c08094cacce46ef85c199ed10e3ee6ad
Katharina Reiner <katharina.reiner@hof-university.de>
2024-05-16 20:56:21 +0200

    feat: single room warnings

--- frontend/HappyPlant/screens/room/SingleRoomWarnings.js
+++ frontend/HappyPlant/screens/room/SingleRoomWarnings.js
@@ -1,7 +1,97 @@
-import { Text } from "react-native-elements"
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native"
+import fetchURL from "./RoomService";
+import { useState, useEffect } from "react";
+import Collapsible from "react-native-collapsible";
+import { LinearGradient } from "expo-linear-gradient";
+import Entypo from "react-native-vector-icons/Entypo";
+import { AssignmentTypeAsVerbTranslations } from "../../utils/EnumTranslations";
+import WarnIcon from "../other/WarnIcon";
+import AssignmentIcon from "../other/AssignmentIcon";
 
-export default function SingleRoomWarnings() {
+export default function SingleRoomWarnings({ room }) {
+    const [plantIds, setPlantIds] = useState();
+    const [assignments, setAssignments] = useState();
+    const [warnings, setWarnings] = useState();
+
+    useEffect(() => {
+        if (room.id) {
+            fetchURL(`/rooms/${room.id}/plants`, 'GET',
+                (plantsInRoom) => {
+                    setPlantIds(plantsInRoom.map(x => x.id));
+                    setWarnings(plantsInRoom.filter(x => !x.hasOptimalLightingCondition).map(x => x.name))
+                }
+            );
+        }
+    }, [room])
+
+    useEffect(() => {
+        fetchURL(`/assignments`, 'GET', (list) => {
+            setAssignments(list.filter(i => plantIds?.includes(i.plantId)))
+        })
+    }, [plantIds])
+
+    const [isCollapsed, setIsCollapsed] = useState(true)
+
     return (
-        <Text>Warnings</Text>
+        <View style={styles.container}>
+            <TouchableOpacity onPress={() => setIsCollapsed(!isCollapsed)} >
+                <LinearGradient colors={['#fdfbef', '#fef1ed']} style={styles.toggleContainer}>
+                    <Text style={styles.containerTitle}>Hinweise</Text>
+                    <Entypo name={isCollapsed ? 'chevron-up' : 'chevron-down'} color={'#98948f'} size={20} />
+                </LinearGradient>
+            </TouchableOpacity>
+            <Collapsible collapsed={isCollapsed}>
+                <LinearGradient colors={['#fdfbef', '#fef1ed']} style={styles.detailContainer}>
+                    {warnings?.length + assignments?.length === 0
+                        ? <Text style={styles.hintItem}>Hier gibt es nichts zu tun.</Text>
+                        : null
+                    }
+                    {warnings?.map(name =>
+                        <View style={styles.hintItem}>
+                            <WarnIcon style={styles.icon} />
+                            <Text numberOfLines={1}>Standort von {name} kann verbessert werden</Text>
+                        </View>
+                    )}
+                    {assignments?.map(a =>
+                        <View style={styles.hintItem}>
+                            <AssignmentIcon style={styles.icon} />
+                            <Text numberOfLines={1}>{a.plantName} muss {AssignmentTypeAsVerbTranslations[a.assignmentType]} werden</Text>
+                        </View>
+                    )}
+                </LinearGradient>
+            </Collapsible>
+
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#fdfbef',
+        borderRadius: 15,
+    },
+    toggleContainer: {
+        borderRadius: 15,
+        padding: 15,
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    containerTitle: {
+        fontSize: 16,
+    },
+    detailContainer: {
+        borderRadius: 15,
+        padding: 15,
+    },
+    hintItem: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        margin: 5,
+    },
+    icon: {
+        paddingRight: 5,
+    }
+})
