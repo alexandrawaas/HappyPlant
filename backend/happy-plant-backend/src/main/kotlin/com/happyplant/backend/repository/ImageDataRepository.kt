31df48e3a0bdc0b1577d610b78619a39ca6cf34e
ralfm <ralf.manig@hof-university.de>
2024-05-21 20:29:30 +0200

    deletes images of user when user is deleted

--- backend/happy-plant-backend/src/main/kotlin/com/happyplant/backend/repository/ImageDataRepository.kt
+++ backend/happy-plant-backend/src/main/kotlin/com/happyplant/backend/repository/ImageDataRepository.kt
@@ -1,7 +1,6 @@
 package com.happyplant.backend.repository
 
 import com.happyplant.backend.model.Image
-import com.happyplant.backend.model.Species
 import org.springframework.data.jpa.repository.JpaRepository
 import java.util.*
 
