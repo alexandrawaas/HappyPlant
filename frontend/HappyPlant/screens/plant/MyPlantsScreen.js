a929dd95e7409b98eb7a4074b2ebb68a8fe3fa3b
awaas <alexandra.waas@hof-university.de>
2024-05-27 21:27:34 +0200

    feat: implemented fetch from backend

--- frontend/HappyPlant/screens/plant/MyPlantsScreen.js
+++ frontend/HappyPlant/screens/plant/MyPlantsScreen.js
@@ -1,11 +1,19 @@
-import {ScrollView, StyleSheet, View} from "react-native";
+import {ScrollView, StyleSheet, Text, View} from "react-native";
 import VerticalPlaceholder from "../../utils/styles/VerticalPlaceholder";
 import AddRoomButton from "../room/AddRoomButton";
 import PlantListItem from "./PlantListItem";
 import {plantMock} from "./PlantMock";
 import {roomMock} from "../room/RoomMock";
+import {useEffect, useState} from "react";
+import fetchURL from "../../utils/ApiService";
 
 export default function MyPlantsScreen({ navigation }) {
+
+    const [plants, setPlants] = useState([]);
+
+    useEffect(() => {
+        fetchURL('/plants', 'GET', setPlants)
+    }, [])
 
     let handleAddPlantClick = () => {
         console.log("TODO: implement add plant dialog")
@@ -13,20 +21,26 @@ export default function MyPlantsScreen({ navigation }) {
 
     return (
         <ScrollView style={styles.container}>
-            <View style={styles.plantContainer}>
-                {plantMock.map(plant =>
-                    <PlantListItem key={plant.id} plant={plant} onPress={(id) => navigation.navigate("Pflanzenprofil", {id: id})} />
-                )}
-                {plantMock.length % 2 == 1
-                    ? <AddRoomButton onClick={handleAddPlantClick} displayAsCard />
-                    : null
-                }
-            </View>
-            {plantMock.length % 2 == 0
-                ? <AddRoomButton onClick={handleAddPlantClick} displayOnNewLine />
-                : null}
-            <VerticalPlaceholder size={130}/>
-        </ScrollView>
+                <View>
+                    <View style={styles.plantContainer}>
+                    {
+                        plants.length === 3
+                                ? <Text style={{alignSelf: "center", textAlign: "center", marginTop: "80%", width: "100%"}}>Keine Pflanzen angelegt</Text>
+                                :
+                        plants.map(plant =>
+                        <PlantListItem key={plant.id} plant={plant} onPress={(id) => navigation.navigate("Pflanzenprofil", {id: id})} />
+                    )}
+                    {plants.length % 2 === 1
+                        ? <AddRoomButton onClick={handleAddPlantClick} displayAsCard />
+                        : null
+                    }
+                    </View>
+                    {plants.length % 2 === 0
+                        ? <AddRoomButton onClick={handleAddPlantClick} displayOnNewLine />
+                        : null}
+                    <VerticalPlaceholder size={130}/>
+                </View>
+            </ScrollView>
     );
 }
 
--- frontend/HappyPlant/screens/room/SingleRoomScreen.js
+++ frontend/HappyPlant/screens/room/SingleRoomScreen.js
@@ -1,11 +1,10 @@
-import {View, Text, StyleSheet, ScrollView } from "react-native";
+import {StyleSheet, ScrollView } from "react-native";
 import { useRoute } from "@react-navigation/native";
 import SingleRoomGrid from "./SingleRoomGrid";
 import SingleRoomInventory from "./SingleRoomInventory";
 import SingleRoomWarnings from "./SingleRoomWarnings";
 import { useEffect, useState } from "react";
 import VerticalPlaceholder from "../../utils/styles/VerticalPlaceholder";
-import { roomMock } from "./RoomMock";
 import fetchURL from '../../utils/ApiService'
 
 export default function SingleRoomScreen({ navigation }) {
