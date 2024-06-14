579d67077b7a54b23ea27e14de08a91f93417df6
Ralf Manig <ralf.manig@hof-university.de>
2024-06-14 13:09:58 +0200

    reset config.js

--- frontend/HappyPlant/config.js
+++ frontend/HappyPlant/config.js
@@ -1,3 +1,3 @@
 // export const API_URL = 'http://192.168.2.130:8080'; // für Android Emulator
-//export const API_URL = 'http://localhost:8080'; // für iOS Simulator
-export const API_URL = 'http://192.168.56.1:8080'; // für Android Emulator
+export const API_URL = 'http://localhost:8080'; // für iOS Simulator
+//export const API_URL = 'http://192.168.56.1:8080'; // für Android Emulator
