a03b643ba937967d55b9c8f59fd1ed8a661e548f
ralfmanig <ralf.manig@hof-university.de>
2024-06-20 11:58:29 +0200

    wip

--- frontend/HappyPlant/screens/global/ImageComponent.js
+++ frontend/HappyPlant/screens/global/ImageComponent.js
@@ -5,36 +5,42 @@ import { getAuthToken } from "../../utils/AuthTokenUtil";
 import { fetchImage } from '../../utils/ApiService';
 
 
-const ImageComponent = ({ imageId, style }) => {
+const ImageComponent = ({ imageId, style, raw = false, imageData }) => {
   const [imageBase64, setImageBase64] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    if (!imageId) {
-      setLoading(false);
-      return;
-    }
+    if (!raw) {
+      if (!imageId) {
+        setLoading(false);
+        return;
+      }
 
-    fetchImage(`/images/${imageId}`, 'GET', null, (response) =>{
+      fetchImage(`/images/${imageId}`, 'GET', null, (response) => {
         setImageBase64(response);
         setLoading(false);
-    })
+      })
+    }
+  }, [imageId, raw]);
 
-    
-  }, [imageId]);
-
-  console.log({uri: imageBase64})
-  if (loading) { 
+  console.log({ uri: imageBase64 })
+  if (loading) {
     return (
       <View>
-        <ActivityIndicator size="large" color="#42f590" style={style ? { ...style, resizeMode: 'contain' } : { ...styles.default, resizeMode: 'contain' }}/>
+        <ActivityIndicator size="large" color="#42f590" style={style ? { ...style, resizeMode: 'contain' } : { ...styles.default, resizeMode: 'contain' }} />
       </View>
     );
   }
 
   return (
-    <View>
-      {imageBase64 && <Image source={{ uri: imageBase64 }} style={style ? { ...style, resizeMode: 'contain' } : { ...styles.default, resizeMode: 'contain' }} />}
+    <View>{raw
+      ? <Image source={imageData} style={{ ...styles.default, ...styles.image, resizeMode: 'contain' }} />
+      : {
+        loading
+          ?<ActivityIndicator size = "large" color = "#42f590" style = { style? { ...style, resizeMode: 'contain'
+      } : {...styles.default, resizeMode: 'contain' }} />
+      : {imageBase64 && <Image source={{ uri: imageBase64 }} style={style ? { ...style, resizeMode: 'contain' } : { ...styles.default, resizeMode: 'contain' }} />}
+            }}
     </View>
   );
 };
