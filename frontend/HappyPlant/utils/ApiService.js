91b1fce3a1746ea006391dbbaffcde52ccad5508
Katharina Reiner <katharina.reiner@hof-university.de>
2024-06-20 10:19:53 +0200

    refac: api service extensions

--- frontend/HappyPlant/utils/ApiService.js
+++ frontend/HappyPlant/utils/ApiService.js
@@ -1,25 +1,30 @@
 import { API_URL } from "../config";
 import { getAuthToken } from "./AuthTokenUtil";
 
+const getResponse = async (url, method='GET', payload=null, needsAuth=true) => {
+    let authToken
+    if(needsAuth) {
+        authToken = await getAuthToken();
+    }
+    const options = {
+        headers: {
+            'Authorization': `Bearer ${authToken}`,
+            'Content-Type': 'application/json'
+        },
+        method: method,
+    };
+
+    if (payload) {
+        options.body = JSON.stringify(payload);
+    };
+
+    const response = await fetch(`${API_URL}${url}`, options);
+    return response
+}
+
 export const fetchURL = async (url, method='GET', payload=null, callback = ()=>{}, needsAuth=true) => {
     try {
-        let authToken
-        if(needsAuth) {
-            authToken = await getAuthToken();
-        }
-        const options = {
-            headers: {
-                'Authorization': `Bearer ${authToken}`,
-                'Content-Type': 'application/json'
-            },
-            method: method,
-        };
-
-        if (payload) {
-            options.body = JSON.stringify(payload);
-        };
-
-        const response = await fetch(`${API_URL}${url}`, options);
+        const response = await getResponse(url, method, payload, needsAuth)
 
         if (response.ok) {
             if(response.status === 204) {
@@ -40,6 +45,30 @@ export const fetchURL = async (url, method='GET', payload=null, callback = ()=>{
         }
     } catch (error) {
         console.error(`Fehler beim Abrufen der Daten (${method} ${url}):`, error);
+        window.alert('Fehler beim Abrufen der Daten');
+        callback([]);
+    }
+}
+
+export const fetchImage = async (url, method='GET', payload=null, callback = ()=>{}, needsAuth=true) => {
+    try {
+        const response = await getResponse(url, method, payload, needsAuth)
+
+        if (response.ok) {
+            const data = await response.blob();
+            if (data.error) {
+                console.error('error: ', data);
+                window.alert(data.error);
+                callback([]);
+            } else {
+                callback(data); 
+            }
+        } else {
+            console.error('Fehler beim Abrufen der Daten:', response.status);
+            callback([]);
+        }
+    } catch (error) {
+        console.error('Fehler beim Abrufen der Daten:', error);
         window.alert('Fehler beim Abrufen der Daten');
         callback([]);
     }
