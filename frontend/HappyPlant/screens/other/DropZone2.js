38aa7e85e8f264b01ca23ccb7370a77afc0113df
Katharina Reiner <katharina.reiner@hof-university.de>
2024-06-24 09:49:43 +0200

    fix: always receive drop zone dimensions

--- frontend/HappyPlant/screens/other/DropZone2.js
+++ frontend/HappyPlant/screens/other/DropZone2.js
@@ -1,17 +1,24 @@
-import React, { forwardRef, useEffect, useRef, useState } from "react";
-import { View, StyleSheet, Text } from "react-native";
+import React, { forwardRef, useEffect } from "react";
+import { View, StyleSheet } from "react-native";
 
-export default DropZone2 = forwardRef(({ style, draggedObject, addMeasures, index, children }, ref) => {
+export default DropZone2 = forwardRef(({ addMeasures, index, children }, ref) => {
     useEffect(() => {
         ref.current.measure((fx, fy, width, height, px, py) => {
-            console.log(fx, fy, width, height, px, py)
             addMeasures({ minX: px, maxX: px + width, minY: py, maxY: py + height }, index)
         })
     }, [ref.current])
 
     return (
-        <View ref={ref} style={style}>
+        <View ref={ref} style={styles.test}>
             {children}
         </View>
     );
 })
+
+// WARN: nicht löschen, sonst geht das ganze drag and drop nicht mehr x.x
+// aus inem grund werden dann die dimensions von ref.current nicht mehr getrackt
+const styles = StyleSheet.create({
+    test: {
+        backgroundColor: 'transparent'
+    },
+});
