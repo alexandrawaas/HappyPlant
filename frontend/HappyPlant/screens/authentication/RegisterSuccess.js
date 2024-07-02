bdc0e0cc59199c20f5d20130a471becf3f68b347
Marina <marina.0.4.05@icloud.com>
2024-07-02 19:24:30 +0200

    style: RegisterSucess.js

--- frontend/HappyPlant/screens/authentication/RegisterSuccess.js
+++ frontend/HappyPlant/screens/authentication/RegisterSuccess.js
@@ -1,16 +1,20 @@
-import {Platform, StyleSheet, Text, TextInput, TouchableOpacity, View} from "react-native";
-import {commonStyles} from "../../utils/styles/CommonStyles";
-import React from "react";
+import { Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import React from 'react';
+import { commonStyles } from '../../utils/styles/CommonStyles';
 
 const RegisterSuccess = ({ navigation }) => {
     return (
         <View style={commonStyles.container}>
-            <Text style={styles.title}>Fast geschafft!</Text>
+
             <View style={styles.background}>
-                <Text style={styles.text}> Bitte überprüfen Sie Ihr Postfach und klicken Sie auf den Bestätigungslink, um Ihre E-Mail-Adresse zu verifizieren und Ihre Registrierung abzuschließen. </Text>
-                <Text style={styles.text2}> E-Mail </Text>
+                <Text style={styles.text}>
+                    Bitte überprüfen Sie Ihr Postfach und klicken Sie auf den Bestätigungslink, um Ihre E-Mail-Adresse zu verifizieren und Ihre Registrierung abzuschließen.
+                </Text>
             </View>
-            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Anmelden')}>
+            <TouchableOpacity
+                style={styles.button}
+                onPress={() => navigation.navigate('Anmelden')}
+            >
                 <Text style={styles.buttonText}>OK</Text>
             </TouchableOpacity>
         </View>
@@ -18,41 +22,12 @@ const RegisterSuccess = ({ navigation }) => {
 };
 
 const styles = StyleSheet.create({
-    title: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        marginBottom: 20,
-        backgroundColor: '#BEF5B5',
-        padding: 40,
-        paddingTop: 120,
-        borderRadius: 20,
-        width: '100%',
-        textAlign: 'center',
-        top: -290,
-    },
     text: {
         fontSize: 16,
         textAlign: 'center',
-    },
-    input: {
-        top: 20,
-        width: '80%',
-        marginTop: 5,
-        marginBottom: 10,
-        padding: 10,
-        borderWidth: 1,
-        borderColor: '#ccc',
-        borderRadius: 12,
-        backgroundColor: '#FDFBEF',
-        shadowOffset: { width: 1, height: 1 },
-        shadowOpacity: 0.2,
-        shadowRadius: 1,
-        elevation: 3,
+        padding: 20,
     },
     button: {
-        top: Platform.OS === 'ios' ? -100 : -80,
-        zIndex: 1,
-        elevation: 5,
         backgroundColor: '#BEF5B5',
         paddingVertical: 10,
         paddingHorizontal: 20,
@@ -60,36 +35,27 @@ const styles = StyleSheet.create({
         shadowOffset: { width: 1, height: 1 },
         shadowOpacity: 0.5,
         shadowRadius: 2,
+        elevation: 5,
     },
     buttonText: {
         color: 'black',
         fontSize: 18,
-    },
-    text2: {
-        top: 20,
-        marginTop: 5,
-        marginBottom: -3,
-        margin: 3,
-        color: '#233D0C',
-        zIndex: 1,
-        textAlign: 'left',
-        width: '100%',
-        marginLeft: 80,
+        fontWeight: 'bold',
     },
     background: {
         backgroundColor: 'white',
-        top: -100,
-        height: 230,
+        paddingVertical: 30,
+        paddingHorizontal: 20,
         borderRadius: 20,
-        width: 360,
+        width: '80%',
         shadowOffset: { width: 1, height: 1 },
         shadowOpacity: 0.4,
         shadowRadius: 3,
         elevation: 5,
         justifyContent: 'center',
         alignItems: 'center',
+        marginBottom: 30,
     },
-
 });
 
 export default RegisterSuccess;
