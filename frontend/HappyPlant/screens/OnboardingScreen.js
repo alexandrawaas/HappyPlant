e60f4ccec105e5cb336d7ec9ee1928306f87d0ae
Katharina Reiner <katharina.reiner@hof-university.de>
2024-07-11 10:52:51 +0200

    refac: dynamic styling on onboarding (smhw the used module is super undynamic so we still have to use top/left/absolutes)

--- frontend/HappyPlant/screens/OnboardingScreen.js
+++ frontend/HappyPlant/screens/OnboardingScreen.js
@@ -1,20 +1,38 @@
 import React from 'react';
 import Onboarding from 'react-native-onboarding-swiper';
-import { Image, StyleSheet, View, Text} from 'react-native';
+import { Image, StyleSheet, View, Text, useWindowDimensions } from 'react-native';
 import { useNavigation } from "@react-navigation/native"
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { LogBox } from 'react-native';
 
-const MyPage = ({ title = '', subtitle = '', image = null }) => (
-    <View style={styles.page}>
-        <Image style={styles.img} source={image} />
-        <Text style={styles.title}>{title}</Text>
-        <Text style={styles.subtitle}>{subtitle}</Text>
-    </View>
-);
+
+const getImagePosition = (windowWidth, windowHeight) => ({
+    width: 1.2 * windowWidth,
+    height: 0.8 * windowHeight,
+    left: 0.07 * windowWidth,
+    top: -0.32 * windowHeight,
+})
+
+const getTitlePosition = (windowWidth, windowHeight) => ({
+    top: 0.66 * windowHeight,
+})
+
+const getSubtitlePosition = (windowWidth, windowHeight, doubleMargin = false) => ({
+    top: 0.66 * windowHeight + (doubleMargin ? 100 : 77)
+})
 
 const MyOnboarding = () => {
     const navigation = useNavigation();
+    const { width, height } = useWindowDimensions();
+
+    const createPage = (title, subtitle, imgSrc, isFirst = false) => {
+        return {
+            backgroundColor: '#BDEEB5',
+            image: <Image style={getImagePosition(width, height)} source={imgSrc} />,
+            title: <Text style={[styles.text, styles.title, getTitlePosition(width, height)]} numberOfLines={2}>{title}</Text>,
+            subtitle: <Text style={[styles.text, styles.subtitle, getSubtitlePosition(width, height, isFirst)]} numberOfLines={3}>{subtitle}</Text>,
+        }
+    }
 
     const handleOnboardingFinish = async () => {
         await AsyncStorage.setItem('hasOnboarded', 'true');
@@ -33,92 +51,26 @@ const MyOnboarding = () => {
             onSkip={handleOnboardingFinish}
             onDone={handleOnboardingFinish}
             pages={[
-                {
-                    backgroundColor: '#BDEEB5',
-                    image: <Image style={styles.img} source={require('../assets/Onboarding/Onboarding1.png')} />,
-                    title: (
-                        <Text style={[styles.title, { top: 520, left: 60 }]}>
-                            Willkommen bei{'\n'}HappyPlant
-                        </Text>
-                    ),
-                    subtitle: (
-                        <Text style={[styles.subtitle, { top: 620, left: 60 }]}>
-                            Pflanzenpflege, smart und einfach.
-                        </Text>
-                    ),
-                },
-                {
-                    backgroundColor: '#BDEEB5',
-                    image: <Image style={styles.img} source={require('../assets/Onboarding/Onboarding2.png')} />,
-                    title: (
-                        <Text style={[styles.title, { top: 520, left: 60 }]}>
-                            To-dos verwalten
-                        </Text>
-                    ),
-                    subtitle: (
-                        <Text style={[styles.subtitle, { top: 620, left: 60 }]}>
-                            Behalte den Überblick über Gießen, {'\n'}Schnitt, Umtopen und Düngen.
-                        </Text>
-                    ),
-                },
-                {
-                    backgroundColor: '#BDEEB5',
-                    image: <Image style={styles.img} source={require('../assets/Onboarding/Onboarding3.png')} />,
-                    title: (
-                        <Text style={[styles.title, { top: 520, left: 60 }]}>
-                            Optimaler Standort
-                        </Text>
-                    ),
-                    subtitle: (
-                        <Text style={[styles.subtitle, { top: 620, left: 60 }]}>
-                            Finde den perfekten Platz {'\n'}für deine Pflanzen.
-                        </Text>
-                    ),
-                },
-                {
-                    backgroundColor: '#BDEEB5',
-                    image: <Image style={styles.img} source={require('../assets/Onboarding/Onboarding4.png')} />,
-                    title: (
-                        <Text style={[styles.title, { top: 520, left: 60 }]}>
-                            Pflanzenwissen
-                        </Text>
-                    ),
-                    subtitle: (
-                        <Text style={[styles.subtitle, { top: 620, left: 60 }]}>
-                            Entdecke verschiedene Arten {'\n'}und erweitere dein Know-how.
-                        </Text>
-                    ),
-                },
+                createPage('Willkommen bei HappyPlant!', 'Pflanzepflege, smart und einfach.', require('../assets/Onboarding/Onboarding1.png'), true),
+                createPage('To-dos verwalten', 'Behalte den Überblick über Gießen, Schneiden, Umtopfen und Düngen.', require('../assets/Onboarding/Onboarding2.png')),
+                createPage('Optimaler Standort', 'Finde den perfekten Platz für deine Pflanzen.', require('../assets/Onboarding/Onboarding3.png')),
+                createPage('Pflanzenwissen', 'Entdecke verschiedene Arten und erweitere dein Know-how.', require('../assets/Onboarding/Onboarding4.png')),
             ]}
         />
     );
 };
 
 const styles = StyleSheet.create({
-    page: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    img: {
-        width: 480,
-        height: 620,
-        left: 30,
-        top: -250,
+    text: {
+        width: '80%',
+        position: 'absolute',
+        color: '#233D0C',
     },
     title: {
         fontSize: 30,
-        position: 'absolute',
-        top: 140,
-        color: '#233D0C',
-        left: 90,
     },
     subtitle: {
         fontSize: 20,
-        position: 'absolute',
-        top: 240,
-        color: '#233D0C',
-        left: 90,
     },
 });
 
